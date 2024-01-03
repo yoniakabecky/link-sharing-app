a40ad595d79d48877f52a102b35a30690a83bc87
Yoni <yoniakabecky@gmail.com>
2024-01-03 12:48:34 +0900

    Add validation for links-editor

--- src/components/links-editor/links-editor.tsx
+++ src/components/links-editor/links-editor.tsx
@@ -1,26 +1,100 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$ } from "@builder.io/qwik";
+import { insert, remove, useForm, valiForm$ } from "@modular-forms/qwik";
 
 import FormWrapper from "~/components/form-wrapper/form-wrapper";
-import LinkItem from "~/components/link-item/link-item";
+import { Icon } from "~/components/icon/icon";
+import {
+  type LinksForm,
+  LinksFormSchema,
+  useLinksFormAction,
+  useLinksFormLoader,
+} from "~/routes/links";
+import { TextField } from "../textfield/textfield";
 import styles from "./links-editor.module.css";
 
 export default component$(() => {
+  const [linksForm, { Form, Field, FieldArray }] = useForm<LinksForm>({
+    loader: useLinksFormLoader(),
+    action: useLinksFormAction(),
+    validate: valiForm$(LinksFormSchema),
+    fieldArrays: ["links"],
+  });
+
   return (
-    <FormWrapper formName="linksForm">
+    <FormWrapper formName="LinksForm">
       <div q:slot="heading">Customize your links</div>
       <span q:slot="description">
         Add/edit/remove links below and then share all profiles with the world!
       </span>
 
-      <div class={styles.scrollArea}>
-        <button class={["button button-outlined", styles.addButton]}>
+      <Form id="LinksForm" class={styles.scrollArea}>
+        <button
+          class={["button button-outlined", styles.addButton]}
+          onClick$={() =>
+            insert(linksForm, "links", {
+              value: { platform: "", link: "" },
+            })
+          }
+        >
           + Add new link
         </button>
 
-        <LinkItem index={1} />
-        <LinkItem index={2} />
-        <LinkItem index={3} />
-      </div>
+        <FieldArray name="links">
+          {(fieldArray) => (
+            <>
+              {fieldArray.items.map((item, index) => (
+                <div class={styles.item} key={item}>
+                  <div class={styles.itemName}>
+                    <Icon name="drag_handle" size={18} />
+                    Link #{index + 1}
+                  </div>
+                  <div
+                    class={styles.removeButton}
+                    onClick$={$(() =>
+                      remove(linksForm, "links", { at: index })
+                    )}
+                  >
+                    Remove
+                  </div>
+
+                  <Field name={`links.${index}.platform`}>
+                    {(field, props) => (
+                      <>
+                        <label for={`platform${index}`}>Platform</label>
+                        <select
+                          {...props}
+                          id={`platform${index}`}
+                          value={field.value}
+                        >
+                          <option value="github">GitHub</option>
+                          <option value="youtube">YouTube</option>
+                          <option value="linkedin">Linkedin</option>
+                          <option value="x">X (Twitter)</option>
+                          <option value="portfolio">Portfolio</option>
+                          <option value="custom">Custom</option>
+                        </select>
+                      </>
+                    )}
+                  </Field>
+
+                  <Field name={`links.${index}.link`}>
+                    {(field, props) => (
+                      <TextField
+                        {...props}
+                        name={`link.${index}`}
+                        label="Link"
+                        value={field.value}
+                        error={field.error}
+                        placeholder="https://www.yoniakabecky.com"
+                      />
+                    )}
+                  </Field>
+                </div>
+              ))}
+            </>
+          )}
+        </FieldArray>
+      </Form>
     </FormWrapper>
   );
 });
--- src/routes/links/index.tsx
+++ src/routes/links/index.tsx
@@ -1,8 +1,46 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import {
+  type InitialValues,
+  formAction$,
+  valiForm$,
+} from "@modular-forms/qwik";
+import type { Input } from "valibot";
+import * as v from "valibot";
 
 import LinksEditor from "~/components/links-editor/links-editor";
 import MockView from "~/components/mock-view/mock-view";
+
+export const LinksFormSchema = v.object({
+  links: v.array(
+    v.object({
+      platform: v.string([v.minLength(1, "Please enter a platform.")]),
+      link: v.string([v.minLength(1, "Please enter a link.")]),
+    })
+  ),
+});
+
+export type LinksForm = Input<typeof LinksFormSchema>;
+
+export const useLinksFormLoader = routeLoader$<InitialValues<LinksForm>>(() => {
+  return {
+    links: [
+      {
+        platform: "github",
+        link: "https://github.com/yoniakabecky",
+      },
+      {
+        platform: "",
+        link: "",
+      },
+    ],
+  };
+});
+
+export const useLinksFormAction = formAction$<LinksForm>((values) => {
+  // Runs on server
+  console.log({ values });
+}, valiForm$(LinksFormSchema));
 
 export default component$(() => {
   return (
