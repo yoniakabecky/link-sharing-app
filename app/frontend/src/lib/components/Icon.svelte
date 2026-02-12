035e2bfdfdfcfb57b0d4682ae8a8343154cf30f1
Yoni <yoniakabecky@gmail.com>
2026-02-13 00:07:11 +0900

    Add Icon component

---
+++ app/frontend/src/lib/components/Icon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { icons } from './icons';
+
+	export type IconName = keyof typeof icons;
+
+	type Props = {
+		name: IconName;
+		size?: number;
+	} & svelteHTML.IntrinsicElements['svg'];
+
+	let { name, size = 24, ...props }: Props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	fill="currentColor"
+	{...props}
+>
+	<title>{name}</title>
+	<path d={icons[name]}></path>
+</svg>
