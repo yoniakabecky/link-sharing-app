dcdf361e1a41bf01cbeb90fa22d789ad1c48cf7e
Yoni <yoniakabecky@gmail.com>
2026-02-13 21:01:04 +0900

    Add Card component

---
+++ app/frontend/src/lib/components/Card.stories.svelte
@@ -0,0 +1,28 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Card from './Card.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Card',
+		component: Card,
+		globals: {
+			backgrounds: { value: 'dark' }
+		}
+	});
+</script>
+
+<Story name="Default">Content only</Story>
+
+<Story name="With header and footer" asChild>
+	<Card>
+		{#snippet header()}
+			<h2>Title</h2>
+		{/snippet}
+
+		Content here
+
+		{#snippet footer()}
+			<p>Footer</p>
+		{/snippet}
+	</Card>
+</Story>
---
+++ app/frontend/src/lib/components/Card.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		header?: Snippet;
+		children: Snippet;
+		footer?: Snippet;
+	} & svelteHTML.IntrinsicElements['div'];
+
+	let { children, header, footer, ...props }: Props = $props();
+</script>
+
+<div class="card" {...props}>
+	{#if header}
+		<header>
+			{@render header()}
+		</header>
+	{/if}
+
+	<main>
+		{@render children()}
+	</main>
+
+	{#if footer}
+		<footer>
+			{@render footer()}
+		</footer>
+	{/if}
+</div>
+
+<style>
+	.card {
+		display: flex;
+		flex-direction: column;
+		background-color: var(--color-card);
+		border-radius: var(--radius-md);
+	}
+
+	.card header,
+	.card main,
+	.card footer {
+		padding-inline: var(--spacing-8);
+		padding-block: var(--spacing-5);
+	}
+
+	.card header {
+		position: sticky;
+		top: 0;
+		border-top-left-radius: var(--radius-md);
+		border-top-right-radius: var(--radius-md);
+		background-color: var(--color-card);
+	}
+
+	.card main {
+		flex: 1;
+		overflow-y: auto;
+	}
+
+	.card footer {
+		position: sticky;
+		bottom: 0;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		border-bottom-left-radius: var(--radius-md);
+		border-bottom-right-radius: var(--radius-md);
+		background-color: var(--color-card);
+		border-top: 1px solid var(--color-border-gray);
+	}
+</style>
