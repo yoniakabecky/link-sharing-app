4a1bf57f5a0270bd7fe680ee34404e7ed9ab4de9
Yoni <yoniakabecky@gmail.com>
2026-03-06 16:53:52 +0900

    Run format

--- app/frontend/.storybook/preview.ts
+++ app/frontend/.storybook/preview.ts
@@ -3,14 +3,14 @@ import '../src/routes/normalize.css';
 import '../src/routes/global.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
 };
 
 export default preview;
